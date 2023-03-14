cd78427b94da57a46941afd8e532890e763ee521
Noel Gitari <gitarinoel@gmail.com>
2023-03-14 11:02:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -2,30 +2,30 @@ function scuberGreetingForFeet(number){
   // Write your code here!
   if (number <= 400){
     return 'This one is on me!';
-  } else if (number >= 2000 && number <2500){
+  } else if (number > 2000 && number <=2500){
     return 'I will gladly take your thirty bucks.';
-  } else if (number >= 2500){
+  } else if (number > 2500){
     return 'No can do.';
   
   }
 }
 
 function ternaryCheckCity(city){
-  return city === 'NYC'?'Ok, sounds good': 'No go';
+  return city === 'NYC'?'Ok, sounds good.': 'No go.';
   
 
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-  switch (name) {
-    case 'generous';
+  switch (tip) {
+    case 'generous':
     return 'Thank you so much.';
     break;
-    case 'not as generous';
+    case 'not as generous':
     return 'Thank you.';
     break;
-    case 'Thanks for everything.';
+    default:
     return 'Bye.';
     break;
 
